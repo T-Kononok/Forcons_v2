94b444b6ae626cbfc324bfb89e7fd311cf152bb1
T-Kononok <t.k.17.11.2000@gmail.com>
2020-06-09 22:31:23 +0300

    созданы пустые кнопки

--- src/frame/MainFrame.java
+++ src/frame/MainFrame.java
@@ -10,19 +10,20 @@ public class MainFrame extends JFrame {
 
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
-    private final ImagePanel panelFull;
+    private ImagePanel panelFull;
 
     private MainFrame() {
 
         setUndecorated(true);
         setExtendedState(MAXIMIZED_BOTH);
 
-        panelFull = addPanelFull();
-        addJournalTable(panelFull);
-        addMenuComboBox(panelFull);
-        addSortButtons(panelFull);
-        addCancelButton(panelFull);
-        addForconsList(panelFull);
+        addPanelFull();
+        addJournalTable();
+        addMenuComboBox();
+        addSortButtons();
+        addCancelButton();
+        addForconsList();
+        addSkillButtons();
 
 
         getContentPane().add(panelFull);
@@ -34,54 +35,53 @@ public class MainFrame extends JFrame {
         panelFull.setImageFile("image/fon.jpg");
     }
 
-    private ImagePanel addPanelFull() {
-        ImagePanel panelFull = new ImagePanel("image/fon.jpg");
+    private void addPanelFull() {
+        panelFull = new ImagePanel("image/fon.jpg");
         panelFull.setLayout(null);
         panelFull.setSize(WIDTH,HEIGHT);
         panelFull.setLocation(0,0);
-        return panelFull;
     }
 
-    private void addJournalTable(JPanel panel) {
+    private void addJournalTable() {
         JTable journalTable = new JTable();
         journalTable.setTableHeader(null);
         journalTable.setBorder(BorderFactory.createEmptyBorder());
-        journalTable.setSize(1050,500);
+        journalTable.setSize(1050,615);
         journalTable.setLocation(5,5);
-        panel.add(journalTable);
+        panelFull.add(journalTable);
     }
 
-    private void addMenuComboBox(JPanel panel) {
+    private void addMenuComboBox() {
         String[] items = {"10 ФМ-2", "10 ФМ-3", "11 ФМ-3", "Форсоны", "Сохранить"};
         JComboBox<String> combo = new JComboBox<>(items);
         combo.setUI(new MenuBasicComboBoxUI());
         combo.setSize(100,25);
         combo.setLocation(1060,5);
-        panel.add(combo);
+        panelFull.add(combo);
     }
 
-    private void addSortButtons(JPanel panel) {
+    private void addSortButtons() {
         JButton sortPointButton = new JButton();
         sortPointButton.setSize(40,25);
         sortPointButton.setLocation(1165,10);
 //        sortPointButton.addActionListener(ev -> );
-        panel.add(sortPointButton);
+        panelFull.add(sortPointButton);
         JButton sortClassButton = new JButton();
         sortClassButton.setSize(40,25);
         sortClassButton.setLocation(1205,10);
 //        sortClassButton.addActionListener(ev -> );
-        panel.add(sortClassButton);
+        panelFull.add(sortClassButton);
     }
 
-    private void addCancelButton(JPanel panel) {
+    private void addCancelButton() {
         JButton cancelButton = new JButton();
         cancelButton.setSize(25,25);
         cancelButton.setLocation(1250,5);
         cancelButton.addActionListener(ev -> dispose());
-        panel.add(cancelButton);
+        panelFull.add(cancelButton);
     }
 
-    private void addForconsList(JPanel panel) {
+    private void addForconsList() {
         Vector<String> vectorForsons = new Vector<>();
         //
         vectorForsons.addElement(",ba,Горшок,3,6");
@@ -98,9 +98,30 @@ public class MainFrame extends JFrame {
 //        forconsListScroll.getVerticalScrollBar().setUI(new MyScrollbarUI());
         forconsListScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         forconsListScroll.setBorder(BorderFactory.createEmptyBorder());
-        forconsListScroll.setSize(215, 500);
+        forconsListScroll.setSize(215, 615 - 30);
         forconsListScroll.setLocation(1060,35);
-        panel.add(forconsListScroll);
+        panelFull.add(forconsListScroll);
+    }
+
+    private void addSkillButtons() {
+        Vector<JButton> skillButtonsVector = new Vector<>();
+        int size = 60;
+        int strut = 40;
+        int x = (WIDTH - size*6 - strut*5)/2;
+        for (int i = 0; i < 6; i++) {
+            skillButtonsVector.add(addOneSkillButton(x,size));
+            x += size+strut;
+        }
+        skillButtonsVector.get(0).addActionListener(ev -> dispose());
+
+    }
+
+    private JButton addOneSkillButton(int x, int size) {
+        JButton skillButton = new JButton();
+        skillButton.setSize(size,size);
+        skillButton.setLocation(x,HEIGHT - skillButton.getHeight() - 20);
+        panelFull.add(skillButton);
+        return skillButton;
     }
 
     public static void main(String[] args) {
